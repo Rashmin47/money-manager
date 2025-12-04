1f41e27a55377496f3e161ea26bd9ae16d6cd353
Rashmin47 <rashminxperience@gmail.com>
2025-12-04 21:05:35 +0545

    Completed Landing Page

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,9 +1,27 @@
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Button } from "./ui/button";
 import Image from "next/image";
 
 const HeroSection = () => {
+  const imageRef = useRef();
+
+  useEffect(() => {
+    const imageElement = imageRef.current;
+
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const scrollThreshold = 100;
+
+      if (scrollPosition > scrollThreshold) {
+        imageElement.classList.add("scrolled");
+      } else {
+        imageElement.classList.remove("scrolled");
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
   return (
     <div className="pb-20 px-4">
       <div className="container mx-auto text-center">
@@ -27,15 +45,17 @@ const HeroSection = () => {
           </Link>
         </div>
 
-        <div>
-          <Image
-            src="/banner.jpeg"
-            width={1280}
-            height={720}
-            alt="Dashboard Preview"
-            className="rounded-lg shadow-2xl border mx-auto"
-            priority
-          />
+        <div className="hero-image-wrapper">
+          <div ref={imageRef} className="hero-image">
+            <Image
+              src="/banner.jpeg"
+              width={1280}
+              height={720}
+              alt="Dashboard Preview"
+              className="rounded-lg shadow-2xl border mx-auto"
+              priority
+            />
+          </div>
         </div>
       </div>
     </div>
